bc06c18261553bb7a4d52b166aad4d5313f516fc
JamesW <chig@foxmail.com>
2019-06-05 18:13:54 +0800

    更新www为pc

--- zbb/www/js/domain.js
+++
@@ -1,9 +0,0 @@
-var passport = passport || window.passport || {},
-    baidu = passport.tangramInst || baidu || window.baidu;
-(function(ns) {
-    ns.apiDomain = {
-        staticDomain : (window.location ? 
-					((window.location.protocol.toLowerCase()=="http:")?"http://passport.bdimg.com/":"https://passport.baidu.com") : 
-					((document.location.protocol.toLowerCase()=="http:")?"http://passport.bdimg.com/":"https://passport.baidu.com"))
-    };
-})(passport);
